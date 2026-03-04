8778109caa7cd4326f2c83dc97817f15df030924
elina-11 <elina.1142k@gmail.com>
2026-03-04 11:20:23 +0100

    Add task_create_firms_and_issuers.py to execute the functions aand save results to BLD folder.

---
+++ src/final_project/analysis/task_create_firms_and_issuers.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import pandas as pd
+import pytask
+
+from final_project.analysis.create_firms_and_issuers import create_firms, create_issuers
+from final_project.config import BLD, SRC
+
+N_FIRMS : int = 50
+N_ISSUERS : int = 75
+FIRMS_SEED : int = 42
+ISSUERS_SEED : int = 24
+
+def task_create_firms(
+        script : Path = SRC / "analysis" / "create_firms_and_issuers.py",
+        produces : Path = BLD / "data" / "firms.pickle", 
+) -> None:
+    """ Create firms and save them to build folder."""
+    firms = create_firms(n_firms = N_FIRMS, seed = FIRMS_SEED)
+    firms.to_pickle(produces)
+
+def task_create_issuers(
+    script: Path = SRC / "analysis" / "create_firms_and_issuers.py",
+    produces: Path = BLD / "data" / "issuers.pickle",
+) -> None:
+    """Create issuers and save them to the build folder."""
+    issuers = create_issuers(n_issuers=N_ISSUERS, seed=ISSUERS_SEED)
+    issuers.to_pickle(produces)
+    
