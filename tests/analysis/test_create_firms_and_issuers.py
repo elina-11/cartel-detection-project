4d456e2fd4cffc803ba2ba5aa41e0e8b37b4a0a8
elina-11 <elina.1142k@gmail.com>
2026-03-04 10:43:37 +0100

    Fix small typos, adding variables to remove chances of errors.

--- tests/analysis/test_create_firms_and_issuers.py
+++ tests/analysis/test_create_firms_and_issuers.py
@@ -3,23 +3,26 @@ import pytest
 
 from final_project.analysis.create_firms_and_issuers import create_firms, create_issuers
 
+N_FIRMS = 20
+N_ISSUERS = 5
+
 
 @pytest.fixture
 def firms_df() -> pd.DataFrame:
     """This function creates a reusable firms DataFrame."""
-    return create_firms(n_firms=50, seed=42)
+    return create_firms(n_firms=N_FIRMS, seed=67)
 
 
 @pytest.fixture
 def issuers_df() -> pd.DataFrame:
     """This function creates a reusable issuers DataFrame."""
-    return create_issuers(n_issuers=30, seed=24)
+    return create_issuers(n_issuers=N_ISSUERS, seed=24)
 
 
-def test_create_firm_structure(firms_df: pd.Dataframe) -> None:
-    assert firms_df.shape == (50, 3)
+def test_create_firm_structure(firms_df: pd.DataFrame) -> None:
+    assert firms_df.shape == (N_FIRMS, 3)
     assert list(firms_df.columns) == ["firm_id", "x", "y"]
-    assert firms_df["firm_id"].tolist() == list(range(50))
+    assert firms_df["firm_id"].tolist() == list(range(N_FIRMS))
 
 
 def test_create_firms_coordinates_range(firms_df: pd.DataFrame) -> None:
@@ -28,11 +31,11 @@ def test_create_firms_coordinates_range(firms_df: pd.DataFrame) -> None:
 
 
 def test_create_issuers_structures(issuers_df: pd.DataFrame) -> None:
-    assert issuers_df.shape == (30, 3)
+    assert issuers_df.shape == (N_ISSUERS, 3)
     assert list(issuers_df.columns) == ["issuer_id", "x", "y"]
-    assert issuers_df["issuer_id"].tolist() == list(range(30))
+    assert issuers_df["issuer_id"].tolist() == list(range(N_ISSUERS))
 
 
-def test_create_issuers_coordinats_range(issuers_df: pd.DataFrame) -> None:
+def test_create_issuers_coordinates_range(issuers_df: pd.DataFrame) -> None:
     assert issuers_df["x"].between(0, 1).all()
     assert issuers_df["y"].between(0, 1).all()
