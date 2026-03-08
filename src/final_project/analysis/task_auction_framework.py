88f7bc57c26a889b044d0e7fc53ec2eae1f2cbf1
elina-11 <elina.1142k@gmail.com>
2026-03-08 16:03:11 +0100

    Add task_auction_framework.py withrequired dependencies till this point.

---
+++ src/final_project/analysis/task_auction_framework.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import pandas as pd
+
+from final_project.analysis.auction_framework import SimulationState, run_simulation
+from final_project.analysis.create_firms_and_issuers import create_firms, create_issuers
+from final_project.config import BLD, SRC
+
+N_FIRMS: int = 50
+N_ISSUERS: int = 75
+FIRMS_SEED: int = 42
+ISSUERS_SEED: int = 24
+SIMULATION_SEED: int = 246
+N_ROUNDS: int = 3
+
+
+def task_run_auction_simulation(
+    depends_on: dict[str, Path] | None = None,
+    produces: Path = BLD / "data" / "simulation_state.pickle",
+) -> None:
+    """This function initialises the simulation state, runs the auction simulation,
+    and saves the resulting state.
+    
+    """
+
+    if depends_on is None:
+        depends_on = {
+            "auction_framework": SRC / "analysis" / "auction_framework.py",
+            "create_firms_and_issuers": SRC
+            / "analysis"
+            / "create_firms_and_issuers.py",
+            "contract_generation": SRC / "analysis" / "contract_generation.py",
+            "participation": SRC / "analysis" / "participation.py",
+            "decision_metrics": SRC / "analysis" / "decision_metrics.py",
+            "firm_decision": SRC / "analysis" / "firm_decision.py",
+            "auction_decisions": SRC / "analysis" / "auction_decisions.py",
+        }
+
+    firms_df = create_firms(n_firms=N_FIRMS, seed=FIRMS_SEED)
+    issuers_df = create_issuers(n_issuers=N_ISSUERS, seed=ISSUERS_SEED)
+
+    state = SimulationState(firms_df=firms_df, issuers_df=issuers_df)
+
+    updated_state = run_simulation(
+        n_rounds=N_ROUNDS,
+        state=state,
+        seed=SIMULATION_SEED,
+    )
+
+    produces.parent.mkdir(parents=True, exist_ok=True)
+
+    pd.to_pickle(updated_state, produces)
